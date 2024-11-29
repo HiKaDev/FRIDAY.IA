f600246e8e6349663655fbd03191686cee897bd7
HiKaDev <karen.vasconcelos@contatoserv.com.br>
2024-11-29 13:57:52 -0400

    Create BotMessage.js

---
+++ src/src/components/BotMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './BotMessage.css';
+
+function BotMessage({ text }) {
+  return (
+    <div className="bot-message">
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default BotMessage;
